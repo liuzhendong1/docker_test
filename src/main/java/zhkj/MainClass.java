ac4e3dc5e5595a01a7d2ddbafad0e10560908f5e
sakxl <liuzhd@zih718.com>
2019-01-18 18:11:55 +0800

    3

---
+++ src/main/java/zhkj/MainClass.java
@@ -0,0 +1,8 @@
+package zhkj;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainClass {
+public static void main(String[] args) {}
+}
