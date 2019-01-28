c4cc254ae613d6649c12cd216ae96450379c3de7
sakxl <liuzhd@zih718.com>
2019-01-28 16:31:50 +0800

    9

--- src/main/java/zhkj/HelloWorld.java
+++ src/main/java/zhkj/HelloWorld.java
@@ -1,21 +1,13 @@
 package zhkj;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
  
-@SpringBootApplication
 @RestController
-public class HelloWorld {
-     @RequestMapping("/")
+@RequestMapping("/docker")
+ public class HelloWorld {
+     @RequestMapping("/hello")
      public String helloDocker(){
          return "hello docker!!!";
-    } 
-     
-  
-     public static void main(String[] args) {
-    	 SpringApplication.run(HelloWorld.class,args);
-     }
-    	
+    }   
  }
