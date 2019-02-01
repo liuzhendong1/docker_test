cae2d8e1948f2b44f26fbb7b98267ec8bf011faa
sakxl <liuzhd@zih718.com>
2019-02-01 09:20:49 +0800

    19

--- src/main/java/zhkj/controller/HelloController.java
+++ src/main/java/zhkj/controller/HelloController.java
@@ -4,10 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
-	
+public class HelloController {	
     @RequestMapping("/hello")
-    public String index() {
+    public String hello() {
         return "Hello docker!!!";
     }
 }
